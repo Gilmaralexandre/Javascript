f005e273cd718228550edd4bed946af1b1192c43
Gilmar <gilmar.alexandre.sc@outlook.com>
2023-10-22 12:25:22 -0300

    Commit Inicial

---
+++ EstruturaDados/type_boolean.js
@@ -0,0 +1,15 @@
+//Operações básicas como soma e subtração é bem lógica
+
+//definir uma variável
+
+const num1 = 10;
+const num2 = 10;
+let cadastroAtivo = true;
+
+//=== comparação
+//console.log(num1 === num2);
+
+const texto1 = "Gilmar";
+const texto2 = "Alexandre";
+
+console.log(texto1 === texto2);
